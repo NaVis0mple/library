70352f9bb4f41475fe451df91a75c4e5459915bb
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-04-28 14:43:16 +0800

    toggle app

---
+++ script.js
@@ -0,0 +1,26 @@
+// loop to  create card
+const myLibrary = []
+function Book (name, arthur, readTime, finished) {
+  this.name = name
+  this.arthur = arthur
+  this.readTime = readTime
+  this.finished = finished
+  this.try = function () {
+    console.log(name)
+  }
+}
+// test manually
+const bookA = new Book('bookOfShadow', 'Peter', '2012', 'y')
+bookA.try()
+// add book by user input to library
+
+// button to add book toggle
+const button = document.getElementById('add')
+const toggle = button.addEventListener('click', (e) => {
+  const form = document.getElementById('form')
+  if (form.className === 'hidden') {
+    form.className = ''
+  } else if (form.className === '') {
+    form.className = 'hidden'
+  }
+})
